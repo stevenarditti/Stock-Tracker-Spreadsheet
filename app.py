a2209004ee1c40c75d561db0cb959af16335ea5a
Steve Arditti <steven.arditti@gmail.com>
2019-12-30 14:46:28 -0500

    basic file reading and workbook init

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 
+from openpyxl import Workbook
 import sys
 
 # True to print debug info, False to ignore
 DEBUG = True
+
+def init_workbook():
+    global companies, wb
+    f = open(src_file, "r+")
+    companies = f.readlines()
+    f.close()
+    wb = Workbook()
+    ws = wb.active
+    ws["A1"] = "Company"
+    ws["B1"] = "Price"
+    ws["C1"] = "Prev Close Price"
+    ws["D1"] = "Market Cap"
+    ws["E1"] = "P/E"
+    wb.save(save_file)
 
 if __name__ == "__main__":
     ii = 0 
@@ -16,7 +31,6 @@ if __name__ == "__main__":
                 save_file = src_file + ".xlsx" 
         ii+=1
     
-    #utils.getStockData("appl")
-    #init_workbook()
+    init_workbook()
     #add_companies()
 
