5c1c8e4532419a4984f7be4a9721396258288b3e
Steve Arditti <steven.arditti@gmail.com>
2019-12-30 18:25:08 -0500

    basic API connection

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 
 from openpyxl import Workbook
+from stock import Stock
 import sys
+
+
+######### Variables
+companies = []
+stocks = set()
+src_file = "companies.txt"
+save_file = "companies.xlsx"
+wb = ""
+cur_row = 2
 
 # True to print debug info, False to ignore
 DEBUG = True
+
+def add_companies():
+    for ticker in companies:
+        stocks.add(Stock(ticker))
+    wb.save(save_file)
 
 def init_workbook():
     global companies, wb
@@ -21,7 +36,7 @@ def init_workbook():
     wb.save(save_file)
 
 if __name__ == "__main__":
-    ii = 0 
+    ii = 0
     while len(sys.argv) > ii:
         if sys.argv[ii] == "-p" and len(sys.argv) > ii + 1:
             src_file = sys.argv[ii+1]
@@ -32,5 +47,7 @@ if __name__ == "__main__":
         ii+=1
     
     init_workbook()
-    #add_companies()
-
+    add_companies()
+    for stock in stocks:
+        print(stock.getName())
+        
--- stock.py
+++ stock.py
@@ -1,4 +1,8 @@
+API_ENDPOINT = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-analysis"
+API_KEY = "c4e383cc40msh3aef1fa6e6bc3edp16c9a8jsnf7ad84b3fe46"
 
+import requests
+import json
 
 class Stock:
 
@@ -8,6 +12,22 @@ class Stock:
 
     def __init__(self, ticker):
         self.ticker = ticker
+        data = self.fetch_data()
+        print(data["price"])
+        print(data["price"]["shortName"])
+        name = data["price"]["shortName"]
 
-    def get_data():
+    def fetch_data(self):
         #api info needed, what is the best way to represent this?
+        response = requests.get(
+            API_ENDPOINT,
+            headers={'X-RapidAPI-Key': API_KEY},
+            params={'symbol': self.ticker}
+        )
+        if (response.status_code == 200):
+            return response.json()
+        else:
+            return json.dumps({})
+
+    def getName(self):
+        return self.name
